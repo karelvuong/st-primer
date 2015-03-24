eefe401d84265078aadc99ce28a983c05ced619e
Karel Vuong <vuongkarel@gmail.com>
2015-03-24 13:55:34 -0700

    Add samples

---
+++ samples/sample.java
@@ -0,0 +1,8 @@
+public enum SimpleSingleton {
+  INSTANCE;
+  public void doSomething() {
+  }
+}
+
+//Call the method from Singleton:
+SimpleSingleton.INSTANCE.doSomething();
---
+++ samples/sample.sql
@@ -0,0 +1,1 @@
+SELECT user_id FROM users WHERE user_id = 1
