f65432b93db63d69a9483cd3b16d942e6a0d4c7e
Karel Vuong <vuongkarel@gmail.com>
2015-03-24 16:51:38 -0700

    Correct samples

--- samples/sample.rb
+++ samples/sample.rb
@@ -1,9 +1,9 @@
 def checkRegistry(line)
-registryHives = ["HKEY_CURRENT_USER","HKEY_CLASSES_ROOT","HKEY_LOCAL_MACHINE"]
+  registryHives = ["HKEY_CURRENT_USER","HKEY_CLASSES_ROOT","HKEY_LOCAL_MACHINE"]
 
-registryHives.each do | hive |
-if line =~ /#{hive}/
-  puts "[+] Registry Access to Hive : #{hive}"
-end
-end
+  registryHives.each do | hive |
+    if line =~ /#{hive}/
+      puts "[+] Registry Access to Hive : #{hive}"
+    end
+  end
 end
