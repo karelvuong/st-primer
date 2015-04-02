9e399c42a1b33a2eae56a42bfa9a70e098e94c27
Karel Vuong <vuongkarel@gmail.com>
2015-04-02 00:12:12 -0700

    Update icons and samples
    Update icons and samples

--- samples/sample.java
+++
@@ -1,8 +0,0 @@
-public enum SimpleSingleton {
-  INSTANCE;
-  public void doSomething() {
-  }
-}
-
-//Call the method from Singleton:
-SimpleSingleton.INSTANCE.doSomething();
--- samples/sample.sql
+++
@@ -1,1 +0,0 @@
-SELECT user_id FROM users WHERE user_id = 1
